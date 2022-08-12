9b484943e26717811c891e6450d610eb8f430493
IrinaNaumenko <irina1280808@gmail.com>
2022-08-12 11:38:59 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var number =5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj ={};
+
+console.log(4/0);
+console.log('string'*9);
+let something;
+console.log(something);
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(person["name"]);
+let arr = ['plum.pmg','orange.jpeg'];
+console.log(arr[0]);
+
+
+//let answer=+prompt("Есть ли вам 18?");
+
+//console.log(typeof(null));
+//console.log("arr"+"-objct");
+//console.log(4+"-objct");
+let incr = 10,
+decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2" == 2);
+
+let isChecked = tru,
+isClose = true;
+console.log(isChecked || !isClose);
+ 
+
+
+
+
+
+
